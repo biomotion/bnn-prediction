739cea19c9420c8353770327f69a8a1cfd8f82e5
biomotion <bensonpuppy1997@gmail.com>
2020-05-17 02:55:45 +0800

    create scripts for docker

---
+++ docker_join.sh
@@ -0,0 +1,14 @@
+ 
+#!/bin/bash
+
+BASH_OPTION=bash
+if [ ! -z "$1" ]; then
+    BASH_OPTION=$@
+fi
+
+echo "Joining docker with command: $BASH_OPTION"
+
+docker exec -it \
+    -e ROS_MASTER_URI=$ROS_MASTER_URI \
+    -e DISPLAY=${DISPLAY} \
+    bnn-predition $BASH_OPTION
